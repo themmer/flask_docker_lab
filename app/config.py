884391e00e76b7f319d3d0db9b2077cadb551a04
Tim Hemmer <themmer@gmail.com>
2017-10-03 00:49:41 +0000

    the 2nd version of our application

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Title of the site
-SITE_TITLE = 'Tims cool Flask Docker Lab'
+SITE_TITLE = 'Tims REALLY cool Flask Docker Lab'
 # The URL for static files/images
 STATIC_URL = 'https://s3-us-west-2.amazonaws.com/flaskdockerlab-static/'
 # The name of the
